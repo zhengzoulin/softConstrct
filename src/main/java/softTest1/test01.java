3897ff9423c28de3636bbcf630f70e40dcb89d1c
zhengzoulin <1469253742@qq.com>
2023-06-13 11:07:47 +0800

    第二次修改

--- src/main/java/softTest1/test01.java
+++ src/main/java/softTest1/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
